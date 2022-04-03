9c6602dd9d61d88741b2dd4336fbf49510acea51
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-04-03 20:37:39 +0300

    cors update

--- index.js
+++ index.js
@@ -25,9 +25,12 @@ const getUser = token => {
 };
 
 const corsOptions = {
-  origin: ['*'],
-  credentials: true,
-};
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "allowedHeaders": ["Content-Type"]
+}
 
 (async () => {
   const app = express();
