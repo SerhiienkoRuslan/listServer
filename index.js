adb69a60a5c921ea63a40689b8631c5b5fcb38f9
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-04-03 21:12:21 +0300

    Config update

--- index.js
+++ index.js
@@ -24,18 +24,12 @@ const getUser = token => {
   }
 };
 
-const corsOptions = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "allowedHeaders": ["Content-Type"]
-}
-
 (async () => {
   const app = express();
   const httpServer = createServer(app);
   const pubsub = new PubSub();
 
-  app.use(cors(corsOptions));
+  app.use(cors());
 
   const schema = makeExecutableSchema({ typeDefs, resolvers });
 
