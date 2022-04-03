40dee07d14c95af0894079c98893d72ca08e1698
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-04-03 20:16:27 +0300

    Update config

--- config/config.js
+++ config/config.js
@@ -8,7 +8,15 @@ const dbDetails = {
   dialect: 'mysql'
 }
 
+const dbDetailsProd = {
+  use_env_variable: true,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: 'mysql'
+}
+
 module.exports = {
   development: dbDetails,
-  production: dbDetails
+  production: dbDetailsProd
 }
