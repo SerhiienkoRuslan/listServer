277fd7ffaf63b9e49e726c0f65e668a3871bc994
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-04-10 21:27:26 +0300

    Config update

--- config/config.js
+++ config/config.js
@@ -1,23 +1,27 @@
 require('dotenv').config()
 
-// for dev
-const dbDetails = {
+// // for dev
+// const dbDetails = {
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   dialect: 'mysql'
+// }
+//
+// // for prod
+// const dbDetailsProd = {
+//   host: 'eu-cdbr-west-02.cleardb.net',
+//   user: 'baef10bb942aa4',
+//   password: '2df4469f',
+//   database: 'heroku_8f8fa8607d3081d',
+//   dialect: 'mysql'
+// }
+
+module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: 'mysql'
-}
-
-const dbDetailsProd = {
-  host: 'eu-cdbr-west-02.cleardb.net',
-  user: 'baef10bb942aa4',
-  password: '2df4469f',
-  database: 'heroku_8f8fa8607d3081d',
-  dialect: 'mysql'
-}
-
-module.exports = {
-  development: dbDetails,
-  production: dbDetailsProd
 }
